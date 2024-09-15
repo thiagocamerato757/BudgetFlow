d8acb9f2958880dc06784acd0741fd8163a6ced2
thiagopc757 <cameratothiago@gmail.com>
2024-09-15 16:13:02 -0300

    model login criado

--- BudgetFlow/Login/models.py
+++ BudgetFlow/Login/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100,help_text="Digite o nome de usuário",primary_key=True)
+    password = models.CharField(max_length=100,help_text="Digite a senha")
+    total_depesas = models.FloatField(default=0)
+    total_receita = models.FloatField(default=0)
+  
+
+
+    def __str__(self):
+        return self.username
+    
+
+class Despesa(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.FloatField(default=0)
+    decricao = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=100)
+    data = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nome
+    
+class Receita(models.Model):
+    nome = models.CharField(max_length=100)
+    valor = models.FloatField(default=0)
+    decricao = models.CharField(max_length=255)
+    categoria = models.CharField(max_length=100)
+    data = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.nome
