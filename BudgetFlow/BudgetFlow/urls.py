136736d6ce82fe168fc96608ebdc0805592e65d5
thiagopc757 <cameratothiago@gmail.com>
2024-09-30 12:23:40 -0300

    retirada de import desnecessário

--- BudgetFlow/BudgetFlow/urls.py
+++ BudgetFlow/BudgetFlow/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path,include
-from Login import views
 from django.contrib import admin
 
 urlpatterns = [
