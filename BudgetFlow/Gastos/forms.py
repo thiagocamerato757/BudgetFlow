007b6707237c75a69366760f1c6417cf78b722de
MTuro <marcosturo69@gmail.com>
2024-10-13 16:14:13 -0300

    fluxo de tela, front-end e validacao do forms

--- BudgetFlow/Gastos/forms.py
+++ BudgetFlow/Gastos/forms.py
@@ -1,16 +1,22 @@
 from django import forms
 from .models import Despesa, Receita
+from datetime import date
 
 class DespesaForm(forms.ModelForm):
     class Meta:
         model = Despesa
         fields = ['descricao', 'valor', 'data', 'categoria']  # Campos do modelo
 
-    # Adicione aqui widgets ou customizações adicionais, se necessário
     descricao = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    valor = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    valor = forms.FloatField(min_value=0, widget=forms.NumberInput(attrs={'class': 'form-control'}))
     data = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
     categoria = forms.ChoiceField(choices=Despesa.CATEGORIAS_DESPESAS, widget=forms.Select(attrs={'class': 'form-control'}))
+
+    def clean_data(self):
+        data = self.cleaned_data.get('data')
+        if data and data > date.today():
+            raise forms.ValidationError("A data não pode ser no futuro.")
+        return data
 
 
 class ReceitaForm(forms.ModelForm):
@@ -18,8 +24,13 @@ class ReceitaForm(forms.ModelForm):
         model = Receita
         fields = ['descricao', 'valor', 'data', 'categoria']  # Campos do modelo
 
-    # Adicione aqui widgets ou customizações adicionais, se necessário
     descricao = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    valor = forms.DecimalField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    valor = forms.FloatField(min_value = 0, widget=forms.NumberInput(attrs={'class': 'form-control'}))
     data = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
     categoria = forms.ChoiceField(choices=Receita.CATEGORIAS_RECEITAS, widget=forms.Select(attrs={'class': 'form-control'}))
+
+    def clean_data(self):
+        data = self.cleaned_data.get('data')
+        if data > date.today():
+            raise forms.ValidationError("A data não pode ser no futuro.")
+        return data
--- BudgetFlow/Gastos/models.py
+++ BudgetFlow/Gastos/models.py
@@ -16,7 +16,7 @@ class Despesa(models.Model):
     
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     descricao = models.CharField(max_length=255)
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    valor = models.DecimalField(max_digits = 10, decimal_places=2)
     data = models.DateField()
     categoria = models.CharField(max_length=20, choices=CATEGORIAS_DESPESAS,default='Outros')                                                                                                                                                                                                                                                                                                                                                                       
 
